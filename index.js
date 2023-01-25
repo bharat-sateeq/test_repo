747116d5954960d55f7b6c40e43fdf1af3806640
bharat-sateeq <109965941+bharat-sateeq@users.noreply.github.com>
2023-01-25 21:54:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const DB_URL = "mongodb+srv://df_user:df_user@sateeq.0wfza.mongodb.net/sateeq?retryWrites=true&w=majority"
+const DB_URL ;
 
 
 mongoose.connect(
